5967e00a7525e9dea74ae414b380c75bfa2f3304
amirmmd <amirbak2424@gmail.com>
2025-11-25 19:34:05 +0330

    make class Person

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+  private   String name;
+    private  String phoneNumber;
+    public Person(String name,String phoneNumber)
+    {
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+    }
+    public void getInfo()
+    {
+
+    }
+    public  String getName()
+    {
+        return this.name;
+    }
+    public  String getPhoneNumber()
+    {
+        return this.phoneNumber;
+    }
+}
