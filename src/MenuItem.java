96b990cb0ab276fa4dc0f62cee46931ea8b660af
amirmmd <amirbak2424@gmail.com>
2025-11-25 22:30:20 +0330

    add Class MenuItem

---
+++ src/MenuItem.java
@@ -0,0 +1,30 @@
+public abstract class MenuItem {
+    private String itemId;
+    private String name;
+    private double price;
+    private String category;
+
+    public MenuItem(String itemId, String name, double price, String category) {
+        this.itemId = itemId;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    public abstract void getDetails();
+    public String getItemId()
+    {
+        return this.itemId;
+    }
+    public String getName()
+    {
+        return getName();
+    }
+    public double getPrice()
+    {
+        return this.price;
+    }
+    public String getCategory()
+    {
+        return this.category;
+    }
+}
